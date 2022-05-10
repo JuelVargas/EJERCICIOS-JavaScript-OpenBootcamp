6fc3114e5c5ac1ae7dc693d7b9185b4dcb9ee775
Juel Vargas <juelvargasrojas@gmail.com>
2022-05-10 17:27:18 -0400

    agregando ejercicios tema 3 estructuras de control

---
+++ 03-estructuras-de-control/factirial-while.js
@@ -0,0 +1,9 @@
+num = 10;
+factorial = 1;
+contador = 1;
+while(contador <= num){
+    factorial *= contador;
+    contador++;
+}
+
+console.log(factorial)
---
+++ 03-estructuras-de-control/factorial-break.js
@@ -0,0 +1,12 @@
+num = 10;
+factorial = 1;
+cont = 1;
+
+while(true){
+    factorial *= cont;
+    if(cont == num) break;
+    
+    cont++
+}
+
+console.log(factorial);
