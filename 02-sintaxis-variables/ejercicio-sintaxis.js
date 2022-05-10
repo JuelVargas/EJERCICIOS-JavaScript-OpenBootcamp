1f52bb64caa9fdd11cc335f6cab428216e56d55f
Juel Vargas <juelvargasrojas@gmail.com>
2022-05-10 16:55:04 -0400

    agregando ejercicio tema 2 sintaxis, variables y palabras reservadas

---
+++ 02-sintaxis-variables/ejercicio-sintaxis.js
@@ -0,0 +1,12 @@
+const lista = [
+    'Juel',
+    21,
+    true,
+    new Date('July 03, 2000'),
+    {
+        titulo: "2001: odisea del espacio",
+        Autor: "Arthur C. Clarke",
+        fecha: 1968,
+        url: "https://www.amazon.es/2001-odisea-espacial-Odisea-SELLER/dp/8497599292"
+    }
+];
