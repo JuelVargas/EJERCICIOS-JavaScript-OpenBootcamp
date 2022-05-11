839e1a098ff7e64b6e1ba48a2a82541f23fcc7f1
Juel Vargas <juelvargasrojas@gmail.com>
2022-05-10 20:32:28 -0400

    agregando ejercicio tema 05 numeros

---
+++ 05-numeros/ejercicio-numeros.js
@@ -0,0 +1,12 @@
+const alturaCentimetros = 170;
+
+const alturaMetros = 1.7;
+
+const peso = 65.4;
+
+const alturaRedondeo = Math.ceil(alturaMetros)
+const pesoRedondeo = Math.floor(peso)
+
+const valIguales = Number.MAX_VALUE + 1 == Number.MAX_VALUE
+
+console.log(valIguales)
